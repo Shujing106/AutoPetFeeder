682ecb17e404776f7a9742c9906ec8ceed3efd47
syan <1135677663@qq.com>
2022-04-15 16:09:25 +0100

    MOTOR

---
+++ motor.cpp
@@ -0,0 +1,46 @@
+#include "motor.h"
+
+void motor::setStep(int a, int b, int c, int d)
+{
+    digitalWrite(IN1, a);
+    digitalWrite(IN2, b);
+    digitalWrite(IN3, c);
+    digitalWrite(IN4, d);
+}
+
+void motor::forward(int t, int steps)
+{
+    int i;
+    steps= 70;
+    for(i = 0; i < steps; i++){
+        setStep(1, 0, 0, 0);
+        delay(t);
+        setStep(0, 1, 0, 0);
+        delay(t);
+        setStep(0, 0, 1, 0);
+        delay(t);
+        setStep(0, 0, 0, 1);
+        delay(t);
+    }
+}
+
+void motor::stop()
+{
+    setStep(0, 0, 0, 0);
+}
+
+void motor::backward(int t, int steps)
+{
+    int i;
+
+    for(i = 0; i < steps; i++){
+        setStep(0, 0, 0, 1);
+        delay(t);
+        setStep(0, 0, 1, 0);
+        delay(t);
+        setStep(0, 1, 0, 0);
+        delay(t);
+        setStep(1, 0, 0, 0);
+        delay(t);
+    }
+}
